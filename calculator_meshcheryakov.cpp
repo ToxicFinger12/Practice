08b574c0d021eec3cc9b98bcd6ee62e0d6c159ae
MeshcheryakovPA <sinidrug@gmail.com>
2022-12-01 21:06:01 +0300

    On branch master new file:   calculator_meshcheryakov.cpp

---
+++ calculator_meshcheryakov.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	cout << "ÊÀËÜÊÓËßÒÎÐ" << endl;
+	cout << "1.Ñëîæèòü" << endl;
+	cout << "2.Âû÷åñòü" << endl;
+	cout << "3.Óìíîæèòü" << endl;
+	cout << "4.Ðàçäåëèòü" << endl;
+	
+	
+}
+
